e23c0291aec2dc6379cc4437b957781d50dd2e31
LinusBolls <82451500+LinusBolls@users.noreply.github.com>
2021-11-26 17:21:06 +0100

    feat: added more compliments

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -8,7 +8,7 @@ Module.register("compliments", {
 	// Module config defaults.
 	defaults: {
 		compliments: {
-			anytime: ["Hey there sexy!"],
+			anytime: ["Hey there sexy!", "Among us! (sus)", "uwu senpai gimme your big fat RAM stick", "don't use windows you virgin lol"],
 			morning: ["Good morning, handsome!", "Enjoy your day!", "How was your sleep?"],
 			afternoon: ["Hello, beauty!", "You look sexy!", "Looking good today!"],
 			evening: ["Wow, you look hot!", "You look nice!", "Hi, sexy!"],
